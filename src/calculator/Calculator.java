bcd5b87acd9780ca966e2d52112f06bce23a5c4f
Jason Wolf <wolf-jason@users.noreply.github.com>
2025-06-28 12:25:28 +0200

    Refactor Calculator class for improved readability and structure

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -28,19 +28,43 @@ public class Calculator {
     public static void main(String[] args) {
         
         /*Deklaration der Attribute und Objekte*/
-	
-        
+        int myFirstNumber;
+        int mySecondNumber;
+        char operator;
+        Scanner sc;
+
         /*Initialisierung der Attribute und Objekte*/
-		
+		myFirstNumber = 0;
+        mySecondNumber = 0;
+        operator = ' ';
+        sc = new Scanner(System.in);
 
         /*Eingabeteil*/
-		
+        System.out.println("\n\t\tCalculator for basic operations (+, -, *, /)");
+
+		System.out.print("\n\nPlease enter the first number: ");
+        myFirstNumber = sc.nextInt();
+        System.out.print("Please enter the second number: ");
+        mySecondNumber = sc.nextInt();
+        System.out.print("\nPlease enter the operator: ");
+        operator = sc.next().charAt(0);
 
         /*Verarbeitungsteil*/
+        if (operator == '+') {
+            System.out.println("\nThe result is: " + (myFirstNumber + mySecondNumber));
+        } else if (operator == '-') {
+            System.out.println("\nThe result is: " + (myFirstNumber - mySecondNumber));
+        } else if (operator == '*') {
+            System.out.println("\nThe result is: " + (myFirstNumber * mySecondNumber));
+        } else if (operator == '/') {
+            System.out.println("\nThe result is: " + (myFirstNumber / mySecondNumber));
+        } else { 
+            System.out.println("\n\nInvalid operator");
+        }
 		
 
         /*Ausgabe*/
 
-
+        sc.close();
     }
 }
